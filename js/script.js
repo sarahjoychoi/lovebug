b3f89008fb00a85f7a4a911c76a27c660d1868bb
sarahjoychoi <129066294+sarahjoychoi@users.noreply.github.com>
2023-04-06 15:25:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -229,7 +229,7 @@ function onWindowResize() {
 
       function animate() {
         requestAnimationFrame(animate);
-        delta = clock.getDelta();
+        delta = clock.getDelta() * 0.25;
   			for ( const mixer of mixers ) mixer.update( delta ); 
         renderer.render(scene, camera);
         controls.update();
