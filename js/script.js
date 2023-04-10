89c70ad918443bda52cface14c5c58d31be266c4
sarahjoychoi <129066294+sarahjoychoi@users.noreply.github.com>
2023-04-10 16:27:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,7 +46,10 @@ let loadedModels = [];
 
         const directionalLight = new THREE.DirectionalLight( 0xffffff, 2.5 ); 
         scene.add(directionalLight);
-
+	
+	      const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambientLight);
+	      
         controls = new THREE.OrbitControls(camera, renderer.domElement);
         controls.enableDamping = true;
         controls.dampingFactor = 0.05;
